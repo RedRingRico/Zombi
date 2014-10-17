0c9c36cd7551c33dd8e8f08f9ae1c659c063c909
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-10-17 12:26:06 +0100

    Added more data types, including Linux-specific data types

--- Source/Common/Headers/DataTypes.hpp
+++ Source/Common/Headers/DataTypes.hpp
@@ -2,6 +2,13 @@
 #define __ZOMBI_DATATYPES_HPP__
 
 #include <stdint.h>
+#if defined ZOMBI_PLATFORM_LINUX_X86_64
+#include <DataTypes_Linux_x86_64.hpp>
+#elif defined ZOMBI_PLATFORM_LINUX_X86_32
+#include <DataTypes_Linux_x86_32.hpp>
+#else
+#error Unknown platform
+#endif
 
 typedef signed char		ZOM_SCHAR8;
 typedef unsigned char	ZOM_UCHAR8;
@@ -12,11 +19,17 @@ typedef int32_t			ZOM_SINT32;
 typedef uint32_t		ZOM_UINT32;
 typedef int64_t			ZOM_SINT64;
 typedef uint64_t		ZOM_UINT64;
+typedef size_t			ZOM_MEMSIZE;
 typedef float			ZOM_FLOAT32;
 typedef double			ZOM_FLOAT64;
 
+typedef ZOM_UINT32	ZOM_BOOL;
+
 const ZOM_UINT32 ZOM_OK		= 0x00000000;
 const ZOM_UINT32 ZOM_FAIL	= 0xFFFFFFFF;
+
+const ZOM_BOOL ZOM_TRUE = 1;
+const ZOM_BOOL ZOM_FALSE = 0;
 
 #endif // __ZOMBI_DATATYPES_HPP__
 
---
+++ Source/Linux/Headers/DataTypes_Linux_x86_64.hpp
@@ -0,0 +1,11 @@
+#ifndef __ZOMBI_DATATYPES_LINUX_X86_64_HPP__
+#define __ZOMBI_DATATYPES_LINUX_X86_64_HPP__
+
+#include <linux/types.h>
+#include <cstddef>
+
+typedef int	ZOM_FILE_DESCRIPTOR;
+const int	ZOM_INVALID_FILE_DESCRIPTOR = -1;
+
+#endif // __ZOMBI_DATATYPES_LINUX_X86_64_HPP__
+
